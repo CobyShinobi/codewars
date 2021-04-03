163161496e1b5b1206db0f43789d5a96b4d8059b
Coby Leavitt <coby.leavitt@gmail.com>
2021-04-03 11:22:41 -0600

    8 kyu from April 3, 2021

---
+++ April2021/April3-2021.js
@@ -0,0 +1,9 @@
+// Your task is to return the correct string using the Template String Feature.
+//
+// You must output a string containing the two strings with the word ```' are '```
+
+
+
+var TempleStrings = function(obj, feature) {
+  return `${obj}` + " are " + `${feature}`
+}
