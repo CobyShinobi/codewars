8d4c50f667147dc02dcc7c3b4013499389c27ba2
Coby Leavitt <coby.leavitt@gmail.com>
2021-05-07 11:56:11 -0600

    8 kyu from May 7, 2021

---
+++ May2021/May7-2021.js
@@ -0,0 +1,7 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list in a sorted manner.
+
+
+
+sortme = function( names ){
+  return names.sort()
+}
