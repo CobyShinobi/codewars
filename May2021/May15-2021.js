a983ecc272931c636eecf7b5ee6d6ab66f0ecb2b
Coby Leavitt <coby.leavitt@gmail.com>
2021-05-15 11:52:31 -0600

    8 kyu from May 15, 2021

---
+++ May2021/May15-2021.js
@@ -0,0 +1,13 @@
+// There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes. At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right).
+//
+// Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.
+
+
+
+const flip=(d, a)=>{
+  if(d === "R") {
+    return a.sort((a, b) => a - b)
+  } else {
+    return a.sort((a, b) => b - a)
+  }
+}
