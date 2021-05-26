9ab1d0eb2a08f4f9eeac4d3358cc7ad7a45e5ab9
Coby Leavitt <coby.leavitt@gmail.com>
2021-05-26 07:11:34 -0600

    8 kyu from May 26, 2021

---
+++ May2021/May26-2021.js
@@ -0,0 +1,5 @@
+// Your function should take two arguments: the size of the wall in millimeters and the size of a pixel in millimeters. It should return True if you can fit an exact number of pixels on the wall, otherwise it should return False. For example is_divisible(4050, 27) should return True, but is_divisible(4066, 27) should return False.
+
+
+
+let isDivisible = (wallLength, pixelSize) => wallLength / pixelSize % 1 == 0
