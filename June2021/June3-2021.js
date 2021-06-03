a9dd2d53e3fdf174662a9d156f7b560e748acf63
Coby Leavitt <coby.leavitt@gmail.com>
2021-06-03 08:18:21 -0600

    8 kyu from June 3, 2021

---
+++ June2021/June3-2021.js
@@ -0,0 +1,5 @@
+// Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+
+
+
+let stringClean = s => s.replace(/[1234567890]/ig, "")
