873bad4f4c836ae4191e73698996f6ae0f00898f
Coby Leavitt <coby.leavitt@gmail.com>
2021-06-08 09:07:15 -0600

    8 kyu from June 8, 2021

---
+++ June2021/June8-2021.js
@@ -0,0 +1,5 @@
+// Write the function circleArea/CircleArea which takes in a Circle object and calculates the area of that circle.
+
+
+
+let circleArea = circle => Math.PI * Math.pow(circle.radius, 2)
