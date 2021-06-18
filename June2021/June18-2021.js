5758b5f3ad7e2cd6cd930a6fafbd2466b815eaa8
Coby Leavitt <coby.leavitt@gmail.com>
2021-06-18 10:16:14 -0600

    8 kyu from June 18, 2021

---
+++ June2021/June18-2021.js
@@ -0,0 +1,13 @@
+// Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+// Note: String will have at least one element; words will always be separated by a space.
+
+
+
+function addLength(str) {
+  let arr = str.split(" "),
+      newArr = []
+  for(let i = 0; i < arr.length; i++) {
+    newArr.push(`${arr[i]} ${arr[i].length}`)
+  }
+  return newArr
+}
