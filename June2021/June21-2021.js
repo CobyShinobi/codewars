0cc5d2cc1457ca51f80033352036e7e96a1989e5
Coby Leavitt <coby.leavitt@gmail.com>
2021-06-21 10:17:07 -0600

    8 kyu from June 21, 2021

---
+++ June2021/June21-2021.js
@@ -0,0 +1,19 @@
+// Create a function that takes a number as an argument and returns a grade based on that number.
+
+
+
+function grader(score) {
+  if(score > 1) {
+    return "F"
+  } else if(score >= 0.9) {
+    return "A"
+  } else if(score >= 0.8) {
+    return "B"
+  } else if(score >= 0.7) {
+    return "C"
+  } else if(score >= 0.6) {
+    return "D"
+  } else {
+    return "F"
+  }
+}
