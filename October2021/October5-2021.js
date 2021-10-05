6fa79f16a833987bbd44fa0a5d57cd9a167177d3
Coby Leavitt <coby.leavitt@gmail.com>
2021-10-05 13:16:49 -0600

    8 kyu from October 5, 2021

---
+++ October2021/October5-2021.js
@@ -0,0 +1,5 @@
+// / You need to write a function that reverses the words in a given string. A word can also fit an empty string.
+
+
+
+let reverse = string => string.split(" ").reverse().join(" ")
